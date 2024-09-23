780b0e16896769fad6c8300ea47f81d819ef4c52
Lawrence <15202114392@163.com>
2024-09-23 22:39:08 +0800

    change name

--- assets/js/reader.js
+++ assets/js/reader.js
@@ -3,6 +3,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const speechForm = document.getElementById('speechForm');
     const textInput = document.getElementById('textInput');
+    const voiceSelect = document.getElementById('voiceSelect');
 
     speechForm.addEventListener('submit', function (event) {
         event.preventDefault(); // 防止表单默认提交行为
@@ -10,12 +11,12 @@ document.addEventListener('DOMContentLoaded', function () {
         const text = textInput.value.trim();
 
         if (text === "") {
-            alert("Enter text!");
+            alert("请输入你想阅读的文本！");
             return;
         }
 
-        // 选择语音类型：美式英语 'US English Female' 或 英式英语 'UK English Female'
-        const selectedVoice = 'UK English Male'; // 你可以根据需要更改
+        // 获取选定的语音类型
+        const selectedVoice = voiceSelect.value;
 
         // 使用 ResponsiveVoice 朗读文本
         responsiveVoice.speak(text, selectedVoice);
