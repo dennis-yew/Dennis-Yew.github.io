0cb31d78f2a0f4020cc7ccb9930b21296db66157
Lawrence <15202114392@163.com>
2024-09-24 00:18:10 +0800

    fix speedRange

--- assets/js/reader.js
+++ assets/js/reader.js
@@ -4,22 +4,29 @@ document.addEventListener('DOMContentLoaded', function () {
     const speechForm = document.getElementById('speechForm');
     const textInput = document.getElementById('textInput');
     const voiceSelect = document.getElementById('voiceSelect');
+    const speedRange = document.getElementById('speedRange');
+    const speedValue = document.getElementById('speedValue');
+
     // 更新语速显示
     speedRange.addEventListener('input', function () {
         // 将滑块的值显示到 span 中
         speedValue.textContent = speedRange.value;
     });
+
     speechForm.addEventListener('submit', function (event) {
         event.preventDefault(); // 防止表单默认提交行为
 
         const text = textInput.value.trim();
+        const selectedVoice = voiceSelect.value;
+        const selectedRate = parseFloat(speedRange.value);
+
+
 
         if (text === "") {
             alert("请输入你想阅读的文本！");
             return;
         }
         // 获取选定的语音类型
-        const selectedVoice = voiceSelect.value;
 
         // 检查 ResponsiveVoice 是否支持所选语音
         if (responsiveVoice.voiceSupport(selectedVoice)) {
